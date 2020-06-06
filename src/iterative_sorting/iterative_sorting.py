88b655a2207cf5ae3a1a5953ce46a019601e5664
kiamco <kiamcoo@gmail.com>
2020-06-06 16:00:26 -0700

    search done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -8,9 +8,16 @@ def selection_sort(arr):
         # (hint, can do in 3 loc)
         # Your code here
 
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
+        temp = arr[i]
+        arr[i] = arr[smallest_index]
+        arr[smallest_index] = temp
+        print(arr, smallest_index, cur_index)
 
     return arr
 
@@ -18,8 +25,14 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    for i in range(0, len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
 
-
+    print(arr)
     return arr
 
 '''
@@ -40,7 +53,43 @@ buckets.
 What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
-    # Your code here
+    max_num = None if maximum is None else maximum
+
+    # find max if maximum is none
+    if maximum is None:
+        max_num = arr[0]
+        for i in range(0, len(arr)):
+            if max_num < arr[i]:
+                max_num = arr[i]
 
 
+    # create count arr
+    count_arr = [0] * (max_num + 1)
+
+    for i in range(0, len(arr) ):
+        count_arr[arr[i]] += 1
+
+    # transform count arr
+    for j in range(1, len(count_arr) ):
+        count_arr[j] = count_arr[j] + count_arr[j - 1]
+    
+    newArr = [0] * len(arr)
+
+    for k in reversed(range(0, len(arr))):
+        count_pointer = len(count_arr) - 1 
+        count_arr[count_pointer] -= 1
+        count_pointer = count_pointer - 1
+        print(count_pointer)
+
+        # newArr[count_arr[k]] = arr[k]
+
+    print(newArr)
+    # create new arr
+
+    # print(count_arr)
     return arr
+
+
+
+arr = [2,5,3,4,1,6,7,7,7,2,2]
+counting_sort(arr)
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,30 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range (0, len(arr)):
+        if target == arr[i]:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    l = 0
+    r = len(arr) - 1
 
-    # Your code here
+    while l <= r:
+        mid = round((l + r) / 2)
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            r = mid - 1
+        else:
+            l = mid + 1
 
 
     return -1  # not found
+
+
+
+arr = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+print(binary_search(arr,6))
